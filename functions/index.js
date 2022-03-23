b7dfee3d33048b781a4357b654962ad1ef20326e
Steve <sholtebeck@gmail.com>
2022-03-22 20:30:19 -1000

    update api function "kupu"

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,63 @@
+// Google Cloud API for rest functions
+const { https } = require("firebase-functions");
+const { initializeApp } = require('firebase-admin/app');
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const admin = require("firebase-admin");
+const dbcol = "butterflies"
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
+initializeApp({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 });
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const snapshot = await admin.firestore().collection("butterflies").get();
+
+  let butterflies = [];
+  snapshot.forEach((doc) => {
+    let id = doc.id;
+    let data = doc.data();
+
+    butterflies.push({ id, ...data });
+  });
+
+  res.status(200).send(JSON.stringify(butterflies));
+});
+
+app.get("/:id", async (req, res) => {
+    const snapshot = await admin.firestore().collection('butterflies').doc(req.params.id).get();
+
+    const butterflyId = snapshot.id;
+    const butterflyData = snapshot.data();
+
+    res.status(200).send(JSON.stringify({id: butterflyId, ...butterflyData}));
+})
+
+app.post("/", async (req, res) => {
+  const butterfly = req.body;
+
+  await admin.firestore().collection("butterflies").add(butterfly);
+
+  res.status(201).send();
+});
+
+app.put("/:id", async (req, res) => {
+    const body = req.body;
+
+    await admin.firestore().collection('butterflies').doc(req.params.id).update(body);
+
+    res.status(200).send()
+});
+
+app.delete("/:id", async (req, res) => {
+    await admin.firestore().collection("butterflies").doc(req.params.id).delete();
+
+    res.status(200).send();
+})
+
+exports.kupu = functions.https.onRequest(app);
