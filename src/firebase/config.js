d412f11d41c89cf2606bcc1ee7f95ff291bfc844
Steve <sholtebeck@gmail.com>
2022-07-29 13:28:27 -1000

    remove fs data

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Kupulele Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjsMqOYErjhwXh8NVZHptUIOPlH9dgbKA",
-  authDomain: "kupulele.firebaseapp.com",
-  projectId: "kupulele",
-  storageBucket: "kupulele.appspot.com",
-  messagingSenderId: "759443667425",
-  appId: "1:759443667425:web:f9a6f06122f702e7bebaa0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
