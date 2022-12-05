c6a2a4702b945d0952f35398d48745f532f1a7cc
Steve <sholtebeck@gmail.com>
2022-12-04 15:03:44 -1000

    add new grid page

---
+++ src/pages/ButterflyGrid.js
@@ -0,0 +1,35 @@
+import "../App.css";
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import NavBar from '../NavBar';
+import { getImages } from "../firebase/firestore";
+import ProfileImage from '../ProfileImage';
+
+function ButterflyGrid() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const loadImages = async () => {
+      const gridImages=await getImages();
+      setImages(gridImages);
+    };
+    loadImages();
+  }, []);
+
+  return (
+    <div className="App">
+    <NavBar />
+    <h1><img src="./favicon.ico" alt="butterfly" /> Butterfly Grid</h1>
+    <div className="grid-5x5">
+       {images.map((image) => {
+        return <div className="grid-item" key={image.id} >
+        <h3><Link to={`/${image.id}`}>{image.id}</Link> - {image.name} ({image.sex[0]},{image.date})</h3>
+         <ProfileImage id={image.id} />
+        </div>;
+      })}
+     </div>
+    </div>
+  );
+} 
+
+export default ButterflyGrid;
